9532cb201e310d1d4dc7f437a3a84a219cf381df
nitinkadam70 <99539536+nitinkadam70@users.noreply.github.com>
2022-04-14 14:02:06 +0530

    commite

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -1,15 +1,38 @@
 
 // keep the add to cart component here
+import { useState } from "react";
 const CartButton = () => {
-    //manage state of the count 
-  return <>
-  {/* add to cart button */}
-  {/* count with - and  + button */}
-  <div>
-      <button>-</button>
-      {/* <p className="count-item">{count}</p> */}
-      <button>+</button>
-  </div>
-  </>;
+  //manage state of the count 
+
+  const [count, setCount] = useState(0);
+  const [showCounter, setShowCounter] = useState(true);
+
+
+  const handelInc = () => {
+    setCount(count + 1)
+  }
+
+  const handleDec = () => {
+    setCount(count - 1)
+  }
+
+  return (
+    <>
+      {/* add to cart button */}
+      {/* count with - and  + button */}
+
+      <div>
+        <button onClick={() => setShowCounter(!showCounter)}>
+          {showCounter ? "Add to Cart" : "Remove"}</button>
+        {showCounter &&
+          <div>
+            <button onClick={handleDec}>-</button>
+            <p className="count-item">{count}</p>
+            <button onClick={handelInc}>+</button>
+          </div>}
+      </div>
+
+    </>
+  );
 };
 export default CartButton
--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import data from '../data.json'
+import GroceryItem from "./GroceryItem"
+
 
 
 
@@ -8,21 +10,14 @@ const GroceryDetails = () => {
     return (
         <>
             <h1>Groceries</h1>
-            <div className="container">
+            <div className="container" >
 
                 {/* map through the data and display the cards */}
                 {data.map((item)=>(
-                    <>
-                    <div>
-                        <p>{item.discount}</p>
-                    <img src={item.imgURL} alt={item.title} />
-                    <h4>{item.title}</h4>
-                    <div>
-                        <h4>{item.sellingPrice}</h4>
-                        <p>M.R.P. {item.mrp}</p>
-                    </div>
-                    </div>
-                    </>
+                 
+                 <GroceryItem title={item.title} discount={item.discount} imgURL={item.imgURL} 
+                 mrp = {item.mrp} price ={item.sellingPrice}/>
+
                 ))}
 
 
